db0333c828c9de0cf2a566ca5a2477404134c938
Mudpadans <philmodel.101@gmail.com>
2023-07-11 22:42:47 -0400

    a commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import './App.css';
+import Square from './Square.jsx';
+
+function App() {
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState(true);
+
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""])
+    setPlayer(true)
+  }
+
+  return (
+    <div className="App">
+      <button onClick={handleClick}>Reset</button>
+      <div className="container">
+        {squares.map((val, index) => {
+          return (
+            <Square 
+              key={index}
+              squares={squares}
+              setSquares={setSquares}
+              player={player}
+              setPlayer={setPlayer}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+
+
+export default App;
